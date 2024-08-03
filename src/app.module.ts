dc1625200ef7b407f9803d01554d9c9be97080ea
fisayomi <fisayomiadebajo@gmail.com>
2024-08-03 07:32:01 +0100

    add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from './typeorm.config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    console.log(process.env.DB_USERNAME);
+  }
+}
---
+++ src/typeorm.config.ts
@@ -0,0 +1,25 @@
+import { DataSource } from 'typeorm';
+import { User } from './modules/user/entities/user.entity';
+import { Parent } from './modules/user/entities/parent.entity';
+import { Teacher } from './modules/user/entities/teacher.entity';
+import { Child } from './modules/user/entities/child.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const databaseUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_USERNAME}`;
+
+// Log the database URL
+console.log('Database URL:', databaseUrl);
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [User, Parent, Teacher, Child], // Ensure this path is correct
+  migrations: ['dist/migrations/*.js'], // Ensure this path is correct
+  synchronize: false,
+});
