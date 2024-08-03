da9be72ae96a3a63233e0b6c1fa690672134827c
fisayomi <fisayomiadebajo@gmail.com>
2024-08-03 07:33:32 +0100

    remove unused line

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { AppDataSource } from './typeorm.config';
 
 dotenv.config();
 
