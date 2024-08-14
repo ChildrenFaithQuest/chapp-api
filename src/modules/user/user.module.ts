4c0a07a24e79dcd41d797f1b2ba9569220203327
fisayomi <fisayomiadebajo@gmail.com>
2024-08-14 14:09:01 +0100

    include password service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -10,10 +10,11 @@ import {
 import { ParentService } from './services/parent.service';
 import { ChildService } from './services/child.service';
 import { TeacherService } from './services/teacher.service';
+import { PasswordService } from '@app-shared/services/password-service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Parent, Teacher, Child])],
-  providers: [ParentService, ChildService, TeacherService],
+  providers: [ParentService, ChildService, TeacherService, PasswordService],
   controllers: [
     ParentDetailsController,
     ChildDetailsController,
