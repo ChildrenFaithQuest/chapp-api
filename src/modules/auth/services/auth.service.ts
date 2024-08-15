496a21c20678301feb69956ec2d73a488c185ea5
fisayomi <fisayomiadebajo@gmail.com>
2024-08-15 13:56:39 +0100

    create auth service

---
+++ src/modules/auth/services/auth.service.ts
@@ -0,0 +1,116 @@
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, EntityManager } from 'typeorm';
+import { Auth } from '../entities/auth.entity';
+import { PasswordService } from '@app-shared/services/password-service';
+import { ParentService } from '@app-modules/user/services/parent.service';
+import { ChildService } from '@app-modules/user/services/child.service';
+import { TeacherService } from '@app-modules/user/services/teacher.service';
+import { RegisterDto } from '../dtos/register.dto';
+import { LoginDto } from '../dtos/login.dto';
+import { UserBaseDto } from '@app-modules/user/dto/user-base';
+import { CreateTeacherDto } from '@app-modules/user/dto/create-teacher.dto';
+import { ParentDetailsDto } from '@app-modules/user/dto/parent-details.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(Auth)
+    private readonly authRepository: Repository<Auth>,
+    private readonly passwordService: PasswordService,
+    private readonly parentService: ParentService,
+    private readonly childService: ChildService,
+    private readonly teacherService: TeacherService,
+  ) {}
+
+  async register(registerDto: RegisterDto): Promise<Auth> {
+    const { email, password, userType, ...userDetails } = registerDto;
+    return this.authRepository.manager.transaction(
+      async (transactionalEntityManager: EntityManager) => {
+        // Check if the email already exists
+        const existingAuth = await transactionalEntityManager.findOne(Auth, {
+          where: { email: registerDto.email },
+        });
+        if (existingAuth) {
+          throw new ConflictException('Email already in use');
+        }
+
+        // Hash the password
+        const hashedPassword =
+          await this.passwordService.hashPassword(password);
+
+        // Create authentication record
+        const auth = transactionalEntityManager.create(Auth, {
+          email,
+          password: hashedPassword,
+          userType, // Assuming userType is part of RegisterDto
+        });
+
+        // Save authentication record
+        await transactionalEntityManager.save(Auth, auth);
+
+        const { firstName, lastName, contact, gender, dateOfBirth } =
+          userDetails;
+
+        // Create user-specific record based on userType
+        let userSpecificRecord;
+        if (userType === 'parent') {
+          userSpecificRecord = await this.parentService.create(
+            {
+              firstName,
+              lastName,
+              contact,
+              gender,
+              dateOfBirth,
+            } as ParentDetailsDto,
+            transactionalEntityManager,
+          );
+          auth.parent = userSpecificRecord;
+        } else if (userType === 'child') {
+          userSpecificRecord = await this.childService.create(
+            { firstName, lastName, gender, dateOfBirth } as UserBaseDto,
+            transactionalEntityManager,
+          );
+          auth.child = userSpecificRecord;
+        } else if (registerDto.userType === 'teacher') {
+          userSpecificRecord = await this.teacherService.create(
+            {
+              firstName,
+              lastName,
+              contact,
+              gender,
+              dateOfBirth,
+            } as CreateTeacherDto,
+            transactionalEntityManager,
+          );
+          auth.teacher = userSpecificRecord;
+        }
+
+        // Save the updated authentication record with user-specific details
+        await transactionalEntityManager.save(Auth, auth);
+
+        return auth;
+      },
+    );
+  }
+
+  async login(loginDto: LoginDto): Promise<Auth> {
+    const auth = await this.authRepository.findOne({
+      where: { email: loginDto.email },
+    });
+    if (
+      !auth ||
+      !(await this.passwordService.comparePassword(
+        loginDto.password,
+        auth.password,
+      ))
+    ) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return auth;
+  }
+}
