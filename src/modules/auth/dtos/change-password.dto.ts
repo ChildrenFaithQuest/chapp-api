557d394bba5884bfaba76c51967ec922077ef861
fisayomi <fisayomiadebajo@gmail.com>
2024-08-22 16:19:54 +0100

    add change password dto

---
+++ src/modules/auth/dtos/change-password.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+
+export class ChangePasswordDto {
+  @IsNotEmpty()
+  @IsString()
+  currentPassword: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @MinLength(8, { message: 'New password must be at least 8 characters long.' })
+  @Matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/, {
+    message:
+      'New password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+  })
+  newPassword: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @MinLength(8)
+  confirmPassword: string;
+}
