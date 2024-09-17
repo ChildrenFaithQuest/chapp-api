a2a074666d67a8a6cf0a1c5ac1e57145316cf6ff
fisayomi <fisayomiadebajo@gmail.com>
2024-09-17 20:21:50 +0100

    refactor updateUser dto

--- src/modules/user/dtos/update-user.dto.ts
+++ src/modules/user/dtos/update-user.dto.ts
@@ -13,30 +13,31 @@ import { Type } from 'class-transformer';
 export class UpdateUserDto {
   @IsOptional()
   @ApiPropertyOptional()
-  contact?: ContactInfoDto;
+  contact: ContactInfoDto;
 
   @IsString()
   @IsOptional()
   @IsNotEmpty()
   @ApiPropertyOptional({ description: 'firstname of the user' })
-  firstName?: string;
+  firstName: string;
 
   @IsString()
   @IsOptional()
   @IsNotEmpty()
   @ApiPropertyOptional()
-  lastName?: string;
+  lastName: string;
 
   @IsNotEmpty()
+  @IsOptional()
   @ApiPropertyOptional()
   @IsEnum(UserGender, {
     message: 'gender must be a valid enum value (female, male)',
   })
-  gender?: UserGender;
+  gender: UserGender;
 
   @IsOptional()
   @ApiPropertyOptional()
   @IsDate()
   @Type(() => Date) // Ensures proper transformation from string to Date
-  dateOfBirth?: Date;
+  dateOfBirth: Date;
 }
