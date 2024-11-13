3ab19b5c3d361343c7006a945f4bf789cb50c1e2
fisayomi <fisayomiadebajo@gmail.com>
2024-11-13 12:01:29 +0100

    update Dtos

--- src/modules/user/dtos/create-user.dto.ts
+++ src/modules/user/dtos/create-user.dto.ts
@@ -1,45 +1,14 @@
-import { UserGender } from '@app-types/module.types';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import {
-  IsString,
-  IsNotEmpty,
-  IsOptional,
-  IsDate,
-  IsEnum,
-} from 'class-validator';
-import { ContactInfoDto } from './contact-info.dto';
+import { OmitType } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { SignupDto } from '@app-modules/auth/dtos/signup.dto';
 
-export class CreateUserDto {
+export class CreateUserDto extends OmitType(SignupDto, [
+  'email',
+  'password',
+  'userType',
+]) {
   @IsOptional()
   @IsNotEmpty()
   @IsString()
-  appwriteId?: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  firstName: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  lastName: string;
-
-  @IsNotEmpty()
-  @ApiProperty()
-  @IsEnum(UserGender, {
-    message: 'gender must be a valid enum value (female, male)',
-  })
-  gender: UserGender;
-
-  @IsOptional()
-  @ApiProperty()
-  @IsDate()
-  @Type(() => Date) // Ensures proper transformation from string to Date
-  dateOfBirth?: Date;
-
-  @IsOptional()
-  @ApiPropertyOptional()
-  contact?: ContactInfoDto;
+  appwriteId: string;
 }
