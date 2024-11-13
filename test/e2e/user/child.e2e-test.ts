6186029e44345b840bee8006288252d7d6050b2a
fisayomi <fisayomiadebajo@gmail.com>
2024-11-13 12:01:56 +0100

    enable tests

--- test/e2e/user/child.e2e-test.ts
+++ test/e2e/user/child.e2e-test.ts
@@ -7,7 +7,7 @@ import { DataSource } from 'typeorm';
 import { Child } from '@app-modules/user/entities/child.entity';
 import { UpdateChildDto } from '@app-modules/user/dtos/update-child.dto';
 
-describe.skip('ChildController (e2e)', () => {
+describe('ChildController (e2e)', () => {
   let app: INestApplication;
   let dataSource: DataSource;
   let childId: string;
