32b1c27a9272088dff85f4c6766600b91149acb4
fisayomi <fisayomiadebajo@gmail.com>
2024-11-15 14:28:17 +0100

    add appwrite role services

--- appwrite/src/services/roles/appwrite-role.service.ts
+++ appwrite/src/services/roles/appwrite-role.service.ts
@@ -1,156 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { AppwriteClientService } from '../appwrite-client.service';
-import { UserType } from '@app-types/module.types';
-import { RoleType } from '@app-types/role.types';
+import { Permission, RoleType } from '@app-types/role.types';
+import { AppwriteUserService } from '../users/appwrite-user.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppwriteRoleService {
-  constructor(private readonly appwriteClientService: AppwriteClientService) {}
-
-  // Map userType to roleName in Appwrite
-  private userTypeToRoleMap: Record<UserType, RoleType> = {
-    [UserType.PARENT]: RoleType.PARENT,
-    [UserType.TEACHER]: RoleType.TEACHER,
-    [UserType.CHILD]: RoleType.CHILD,
-  };
-
-  async getRoleIdByUserType(userType: UserType): Promise<string | null> {
-    const database = this.appwriteClientService.getDatabaseService();
-    const roleName = this.userTypeToRoleMap[userType];
-    if (!roleName) {
-      throw new Error(`Role for userType ${userType} not found`);
-    }
-
-    if (
-      !process.env.APPWRITE_USER_ROLES_COLLECTION_ID ||
-      !process.env.APPWRITE_DATABASE_ID
-    ) {
-      throw new Error(
-        'USER_ROLES_COLLECTION_ID environment variable is not defined',
-      );
-    }
-
-    try {
-      const roleDocument = await database.listDocuments(
-        process.env.APPWRITE_DATABASE_ID,
-        process.env.APPWRITE_USER_ROLES_COLLECTION_ID,
-        [`name=${roleName}`],
-      );
-
-      if (roleDocument.total === 0) {
-        throw new Error(`Role ${roleName} not found`);
-      }
-
-      const role = roleDocument.documents[0];
-      return role.$id;
-    } catch (error) {
-      console.error('Error retrieving roleId:', error);
-      return null;
-    }
-  }
-  // Function to assign a role to a user
-  async assignRoleToUserByRoleName({
-    userId,
-    roleName,
-  }: {
-    userId: string;
-    roleName: RoleType;
-  }): Promise<void> {
-    const database = this.appwriteClientService.getDatabaseService();
-    const roleId = await this.getRoleIdByName(roleName);
-    if (!roleId) {
-      throw new Error(`Could not find roleId for role ${roleName}`);
-    }
-
-    try {
-      if (
-        !process.env.APPWRITE_USER_ROLES_COLLECTION_ID ||
-        !process.env.APPWRITE_DATABASE_ID
-      ) {
-        throw new Error(
-          'USER_ROLES_COLLECTION_ID environment variable is not defined',
-        );
-      }
-      // Step 3: Create a document in the UserRoles collection to link the user with the role
-      await database.createDocument(
-        process.env.APPWRITE_DATABASE_ID, // The collection ID for user roles
-        process.env.APPWRITE_USER_ROLES_COLLECTION_ID, // The collection ID for user roles
-        'unique()', // Unique document ID
-        {
-          userId,
-          roleId,
-        },
-      );
-      console.log('Role assigned in UserRoles collection:', roleId);
-    } catch (error) {
-      console.error('Error assigning role to user:', error);
-    }
-  }
-
-  async assignRoleToUserByUserType(
-    userId: string,
-    userType: UserType,
-  ): Promise<void> {
-    const database = this.appwriteClientService.getDatabaseService();
-
-    const roleId = await this.getRoleIdByUserType(userType);
-    if (!roleId) {
-      throw new Error(`Could not find roleId for userType ${userType}`);
-    }
-
-    if (
-      !process.env.APPWRITE_USER_ROLES_COLLECTION_ID ||
-      !process.env.APPWRITE_DATABASE_ID
-    ) {
-      throw new Error(
-        'USER_ROLES_COLLECTION_ID environment variable is not defined',
-      );
-    }
-
-    try {
-      const existingRoles = await database.listDocuments(
-        process.env.APPWRITE_DATABASE_ID,
-        process.env.APPWRITE_USER_ROLES_COLLECTION_ID,
-        [`userId=${userId}`, `roleId=${roleId}`],
-      );
-
-      if (existingRoles.total > 0) {
-        console.log(`User ${userId} already has the role ${userType}`);
-        return;
-      }
-
-      // Assign role by creating a document linking userId and roleId
-      await database.createDocument(
-        process.env.APPWRITE_DATABASE_ID,
-        process.env.APPWRITE_USER_ROLES_COLLECTION_ID,
-        'unique()',
-        {
-          userId,
-          roleId,
-        },
-      );
-
-      console.log(`Role ${roleId} assigned to user ${userId}`);
-    } catch (error) {
-      console.error('Error assigning role to user:', error);
-      throw new Error('Could not assign role to user');
-    }
+  private readonly databaseId: string;
+  private readonly rolesCollectionId: string;
+  private readonly permissionsCollectionId: string;
+  private readonly rolePermissionsCollectionId: string;
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly appwriteClientService: AppwriteClientService,
+    private readonly appwriteUserService: AppwriteUserService,
+  ) {
+    this.databaseId =
+      this.configService.get<string>('APPWRITE_DATABASE_ID') || '';
+    this.rolesCollectionId =
+      this.configService.get<string>('APPWRITE_ROLES_COLLECTION_ID') || '';
+    this.permissionsCollectionId =
+      this.configService.get<string>('APPWRITE_PERMISSIONS_COLLECTION_ID') ||
+      '';
+    this.rolePermissionsCollectionId =
+      this.configService.get<string>(
+        'APPWRITE_ROLE_PERMISSIONS_COLLECTION_ID',
+      ) || '';
   }
 
   async getRoleIdByName(roleName: RoleType): Promise<string | null> {
     const database = this.appwriteClientService.getDatabaseService();
-    if (
-      !process.env.APPWRITE_DATABASE_ID ||
-      !process.env.APPWRITE_ROLES_COLLECTION_ID
-    ) {
-      throw new Error(
-        'APPWRITE_PROJECT_ID or APPWRITE_ROLES_COLLECTION_ID environment variable is not defined',
-      );
-    }
     try {
       const roleDocument = await database.listDocuments(
-        process.env.APPWRITE_DATABASE_ID,
-        process.env.APPWRITE_ROLES_COLLECTION_ID,
+        this.databaseId,
+        this.rolesCollectionId,
         [`name=${roleName}`],
       );
 
@@ -163,6 +46,80 @@ export class AppwriteRoleService {
     } catch (error) {
       console.error('Error retrieving roleId:', error);
       return null;
+    }
+  }
+
+  async getUserRoles(
+    userId: string,
+  ): Promise<{ roleIds: string[]; roles: RoleType[] }> {
+    const database = this.appwriteClientService.getDatabaseService();
+    try {
+      const userProfile = await this.appwriteUserService.getUserProfile(userId);
+      const roleIds = userProfile?.roleIds || []; // assuming `roleIds` is an array
+      const roles = await Promise.all(
+        roleIds.map(async (roleId: string) => {
+          const role = await database.getDocument(
+            this.databaseId,
+            this.rolesCollectionId,
+            roleId,
+          );
+          return role.name;
+        }),
+      );
+      return { roleIds, roles };
+    } catch (error) {
+      console.error('Error fetching user roles:', error);
+      throw new Error('Failed to retrieve user roles');
+    }
+  }
+
+  async getUserRolesAndPermissions(
+    userId: string,
+  ): Promise<{ roles: RoleType[] | null; permissions: Permission[] }> {
+    try {
+      const { roleIds, roles } = await this.getUserRoles(userId);
+      const permissions = await this.getUserPermissions(roleIds);
+      return { roles, permissions };
+    } catch (error) {
+      console.error('Error fetching user roles and permissions:', error);
+      throw new Error('Failed to retrieve user roles and permissions');
+    }
+  }
+
+  async getUserPermissions(roleIds: string[]): Promise<Permission[]> {
+    const database = this.appwriteClientService.getDatabaseService();
+    if (roleIds.length === 0) return [];
+    try {
+      // Fetch role permissions
+      const permissionIds = await Promise.all(
+        roleIds.map(async (roleId: string) => {
+          const result = await database.listDocuments(
+            this.databaseId,
+            this.rolePermissionsCollectionId,
+            [`equal("roleId", "${roleId}")`],
+          );
+          return result.documents.map((doc) => doc.permissionId);
+        }),
+      );
+
+      const uniquePermissionIds = [...new Set(permissionIds.flat())];
+
+      // Fetch permission names
+      const permissions = await Promise.all(
+        uniquePermissionIds.map(async (permissionId: string) => {
+          const permission = await database.getDocument(
+            this.databaseId,
+            this.permissionsCollectionId,
+            permissionId,
+          );
+          return permission.name;
+        }),
+      );
+
+      return permissions;
+    } catch (error) {
+      console.error('Error fetching user permissions:', error);
+      throw new Error('Failed to retrieve user permissions');
     }
   }
 }
