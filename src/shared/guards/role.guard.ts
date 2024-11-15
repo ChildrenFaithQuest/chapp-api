5cbec61ca5e61409a01e6a92edbf99cb29179a16
fisayomi <fisayomiadebajo@gmail.com>
2024-11-15 14:29:57 +0100

    update role guard

--- src/shared/guards/role.guard.ts
+++ src/shared/guards/role.guard.ts
@@ -1,5 +1,3 @@
-import { Auth } from '@app-modules/auth/entities/auth.entity';
-import { Role } from '@app-modules/role/entities/role.entity';
 import { Permission, RoleType } from '@app-types/role.types';
 import {
   Injectable,
@@ -9,10 +7,15 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { PERMISSIONS_KEY, ROLES_KEY } from './role.decorator';
+import { CustomRequest } from '@app-types/express-request.types';
+import { AppwriteRoleService } from '@app-root/appwrite/src/services/roles/appwrite-role.service';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly appwriteRoleService: AppwriteRoleService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<RoleType[]>(
@@ -28,40 +31,39 @@ export class RoleGuard implements CanActivate {
       return true; // No roles or permissions are required
     }
 
-    const request = context.switchToHttp().getRequest();
-    const user: Auth = request.user;
+    const request: CustomRequest = context.switchToHttp().getRequest();
+    const userId = request.user?.$id;
 
-    if (!user || !user.roles) {
-      throw new ForbiddenException(
-        'You do not have the necessary permissions or roles.',
+    if (!userId) {
+      throw new ForbiddenException('User not authenticated.');
+    }
+
+    try {
+      const { roles, permissions } =
+        await this.appwriteRoleService.getUserRolesAndPermissions(userId);
+      const hasRole =
+        roles?.some((role) => requiredRoles.includes(role)) ?? false;
+
+      if (!hasRole) {
+        throw new ForbiddenException(
+          'You do not have the necessary permissions or roles.',
+        );
+      }
+
+      const hasPermission = requiredPermissions.some((permission) =>
+        permissions.includes(permission),
       );
+
+      if (!hasPermission) {
+        throw new ForbiddenException(
+          'You do not have the necessary permissions.',
+        );
+      }
+
+      return hasRole && hasPermission;
+    } catch (error) {
+      console.error('Error in RoleGuard:', error);
+      throw new ForbiddenException('Access denied.');
     }
-
-    const hasRole = requiredRoles
-      ? user.roles.some((role) => requiredRoles.includes(role.name))
-      : true;
-
-    if (!hasRole) {
-      throw new ForbiddenException('User does not have the required role');
-    }
-
-    // Flatten the user's roles and their permissions
-    const userPermissions = user.roles.flatMap(
-      (role: Role) => role.permissions,
-    );
-
-    const hasPermission = requiredPermissions.some((permission) =>
-      userPermissions.includes(permission),
-    );
-
-    if (!hasPermission) {
-      throw new ForbiddenException(
-        'You do not have the necessary permissions.',
-      );
-    }
-
-    // return hasRole && hasPermission;
-
-    return hasPermission;
   }
 }
