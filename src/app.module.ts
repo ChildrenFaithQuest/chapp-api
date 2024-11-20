b33d301ac4b479969a04b6156652d9fabb37fe8e
fisayomi <fisayomiadebajo@gmail.com>
2024-11-20 12:35:24 +0100

    refine typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
 import * as path from 'path';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { AppDataSource } from './typeorm.config';
-import { ConfigModule } from '@nestjs/config';
-import { ClassModule } from './modules/class/class.module';
-import { AttendanceModule } from './modules/attendance/attendance.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { UserModule } from '@app-modules/user';
 import { AuthModule } from '@app-modules/auth/auth.module';
 import { OrgModule } from '@app-modules/organization/organization.module';
 import { RoleModule } from '@app-modules/role/role.module';
-import { AuthMiddleware } from './middlewares/auth.middleware';
 import { UsersController } from '@app-modules/user/controllers/user.controller';
 import { ChildDetailsController } from '@app-modules/user/controllers/child.controller';
-import { envValidationSchema } from './config/env.validation';
 import { AppwriteModule } from '@app-root/appwrite/appwrite.module';
+import { ClassModule } from '@app-modules/class/class.module';
+import { AttendanceModule } from '@app-modules/attendance/attendance.module';
+
+import { AppController } from './app.controller';
+import { AuthMiddleware } from './middlewares/auth.middleware';
+import { createAppDataSource } from './typeorm.config';
+import { AppService } from './app.service';
+import { envValidationSchema } from './config/env.validation';
 
 const env = process.env.NODE_ENV || 'development'; // Default to 'development' if NODE_ENV is undefined
 
@@ -31,9 +32,15 @@ const env = process.env.NODE_ENV || 'development'; // Default to 'development' i
         allowUnknown: true, // Ignore unknown environment variables
       },
     }),
-    TypeOrmModule.forRoot({
-      ...AppDataSource.options,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule to inject ConfigService
+      inject: [ConfigService], // Inject ConfigService into the factory
+      useFactory: async (configService: ConfigService) => {
+        const dataSourceOptions = createAppDataSource(configService);
+        return {
+          ...dataSourceOptions.options,
+        };
+      },
     }),
     AppwriteModule,
     UserModule,
@@ -49,7 +56,8 @@ const env = process.env.NODE_ENV || 'development'; // Default to 'development' i
   exports: [],
 })
 export class AppModule implements NestModule {
-  constructor(private dataSource: DataSource) {}
+  constructor(private readonly configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(AuthMiddleware)
